3695b116d55eb74807883f0ae213c65ee1ffca01
moavalos <mora_m_avalos@hotmail.com>
2024-10-16 13:08:49 -0300

    agrego anotacion

--- src/main/java/org/twelve/infraestructura/RepositorioUsuarioImpl.java
+++ src/main/java/org/twelve/infraestructura/RepositorioUsuarioImpl.java
@@ -11,6 +11,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository("repositorioUsuario")
+@Transactional
 public class RepositorioUsuarioImpl implements RepositorioUsuario {
 
     private SessionFactory sessionFactory;
